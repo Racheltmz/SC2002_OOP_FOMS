a384abe0aa7461d57eccb509cb64dce678673b62
Rachel Tan <racheltanmz@gmail.com>
2024-03-26 15:18:41 +0800

    Removed create branch

--- FOMS/src/Management/Branch.java
+++ FOMS/src/Management/Branch.java
@@ -45,25 +45,5 @@ public class Branch {
     public void setLocation(String location) {
         this.location = location;
     }
-
-    public Branch createBranch()
-    {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.print("Enter branch name: ");
-            String name = sc.nextLine();
-            System.out.print("Enter staff quota: ");
-            int quota = sc.nextInt();
-            while (!(1 <= quota && quota <= 15)) {
-                System.out.println("Invalid staff quota! Please re-enter.");
-                System.out.print("Enter staff quota: ");
-                quota = sc.nextInt();
-            }
-            System.out.print("Enter branch location: ");
-            String location = sc.next();
-            location += sc.nextLine();
-            Branch branch = new Branch(name, location, quota);
-            System.out.printf("New branch created. Its details are:\nName: %s\nStaff quota: %d\nManager Quota: %d\nLocation: %s\n", name, quota, branch.getManagerQuota(), location);
-            return branch;
-        }
-    }
+    
 }
