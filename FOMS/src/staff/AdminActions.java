8200aa0ca5a3f8f4ce29556140f99161b760d263
snnjana <SHANMUGASUNDARAMSANJANA@GMAIL.COM>
2024-04-15 01:20:03 +0800

    data persistence for pymt & customer ui

--- FOMS/src/staff/AdminActions.java
+++ FOMS/src/staff/AdminActions.java
@@ -218,7 +218,7 @@ public class AdminActions {
     }
 
     protected void removePayment() {
-
+        PaymentDirectory.removePaymentMtd(ADMIN);
     }
 
 //    protected String addPayment(String method){
