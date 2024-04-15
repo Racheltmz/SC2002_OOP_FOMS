17d9bf7b1e7fae83f79afb760f9e98eb3f7b7c19
afreen rafi <afreenrafi151@gmail.com>
2024-04-15 23:01:11 +0800

    refactor_activeadmin

--- FOMS/src/authorisation/ActiveAdmin.java
+++ FOMS/src/authorisation/ActiveAdmin.java
@@ -33,52 +33,86 @@ public class ActiveAdmin implements IActiveUser {
         System.out.println("-".repeat(30));
         System.out.println("Please select option (3 to quit): ");
         System.out.println("1. Change Password\n2. Logout");
-        int staffChoice = sc.nextInt();
+        int categoryChoice = sc.nextInt();
         sc.nextLine();
-        // TODO: AFREEN: can we make sub switch statements so we further divide the functionality
-        switch (staffChoice) {
+        // TODO: AFREEN: can we make sub switch statements so we further divide the functionality(done)
+        switch(categoryChoice){
             case 1:
-                // add staff
-                this.getActiveStaff().addStaff(this.activeStaff.getRole());
-                break;
+                System.out.print("1. Add Staff \n2. Update Staff\n3. Remove Staff");
+                int staffChoice = sc.nextInt();
+                sc.nextLine();
+                switch (staffChoice) {
+                    case 1:
+                        // add staff
+                        this.getActiveStaff().addStaff(this.activeStaff.getRole());
+                        break;
+                    case 2:
+                        // edit staff
+                        this.getActiveStaff().updateStaff(this.activeStaff.getRole());
+                        break;
+                    case 3:
+                        // remove staff
+                        this.getActiveStaff().removeStaff(this.activeStaff.getRole());
+                        break;
+                    default:
+                        System.out.print("Invalid choice, please re-enter: ");
+                        break; 
+                }
+                break; 
             case 2:
-                // edit staff
-                this.getActiveStaff().updateStaff(this.activeStaff.getRole());
-                break;
-            case 3:
-                // remove staff
-                this.getActiveStaff().removeStaff(this.activeStaff.getRole());
-                break;
-            case 4:
-                this.getActiveStaff().filterStaff(this.activeStaff.getRole());
-                break;
-            case 5:
-                this.getActiveStaff().addBranch(this.activeStaff.getRole());
-                break;
-            case 6:
-                this.getActiveStaff().closeBranch(this.activeStaff.getRole());
-                break;
-            case 7:
-                this.getActiveStaff().promoteStaff(this.activeStaff.getRole());
-                break;
-            case 8:
-                this.getActiveStaff().transferStaff(this.activeStaff.getRole());
-                break;
-            case 9:
-//                this.getActiveStaff().addPayment(this.activeStaff.getRole());
-                break;
-            case 10:
-//                this.getActiveStaff().removePayment(this.activeStaff.getRole());
-                break;
-            case 11:
-                this.getActiveStaff().changePassword();
-                break;
-            case 12:
-                this.logout();
-                break;
-            default:
-                System.out.println("Invalid choice, please re-enter: ");
-                break;
+                System.out.print("1. Add branch\n2. Close Branch");
+                int branchChoice = sc.nextInt(); 
+                sc.nextLine();
+                switch(branchChoice){
+                    case 1:
+                    this.getActiveStaff().addBranch(this.activeStaff.getRole());
+                    break;
+                case 2:
+                    this.getActiveStaff().closeBranch(this.activeStaff.getRole());
+                    break;
+                default:
+                    System.out.println("Invalid choice, please re-enter: ");
+                    break;
+            }
+            break;
+        case 3: // Staff Changes
+            System.out.println("1. Promote Staff\n2. Transfer Staff");
+            int changeChoice = sc.nextInt();
+            sc.nextLine();
+            switch (changeChoice) {
+                case 1:
+                    this.getActiveStaff().promoteStaff(this.activeStaff.getRole());
+                    break;
+                case 2:
+                    this.getActiveStaff().transferStaff(this.activeStaff.getRole());
+                    break;
+                default:
+                    System.out.println("Invalid choice, please re-enter: ");
+                    break;
+            }
+            break;
+        case 4: // Miscellaneous
+            System.out.println("1. Change Password\n2. Logout");
+            int miscChoice = sc.nextInt();
+            sc.nextLine();
+            switch (miscChoice) {
+                case 1:
+                    this.getActiveStaff().changePassword();
+                    break;
+                case 2:
+                    this.logout();
+                    break;
+                default:
+                    System.out.println("Invalid choice, please re-enter: ");
+                    break;
+            }
+            break;
+        case 5: // Quit
+            System.out.println("Exiting...");
+            break;
+        default:
+            System.out.println("Invalid choice, please re-enter: ");
+            break;
         }
     }
 }
